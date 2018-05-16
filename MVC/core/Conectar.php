7ca6e7599a306ea05247cd6d0d572c015a3b3401
ErickZc <38260845+ErickZc@users.noreply.github.com>
2018-05-15 22:14:29 -0600

    MVC

---
+++ MVC/core/Conectar.php
@@ -0,0 +1,45 @@
+<?php
+
+class Conectar
+{
+	private $driver;
+	private $host;
+	private $user;
+	private $pass;
+	private $database;
+	private $charset;
+
+	function __construct()
+	{
+		//arreglo con la informacion de conexion
+
+		$dataCon=require_once 'config/database.php';
+
+		$this->driver=$dataCon['driver'];
+		$this->host=$dataCon['host'];
+		$this->user=$dataCon['user'];
+		$this->pass=$dataCon['pass'];
+		$this->database=$dataCon['database'];
+		$this->charset=$dataCon['charset'];
+
+	}
+
+	//Metodo de conexion
+
+	public function conexion()
+	{
+		if ($this->driver=='mysql'|| $this->driver==null) {
+			$con= new mysqli($this->host,$this->user,$this->pass,$this->database);
+
+			//seteamos el tipo de cojetamiento
+
+			$con->query("SET NAMES'".$this->charset."'");
+
+			
+		}
+		return $con;
+		
+	}
+	
+}
+?>
